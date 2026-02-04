958ecad24db9777f1383b414c5d4a2109e1161f4
Viktor <vn264@student.aru.ac.uk>
2026-02-04 09:16:21 +0000

    .

---
+++ SquareAndCircle.cs
@@ -0,0 +1,43 @@
+﻿// Simple application to draw rectangle and circle objects
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace CGP
+{
+    public partial class SquareAndCircle : Form
+    {
+        Rectangle aCircle;
+        Rectangle aSquare;
+
+        public SquareAndCircle()
+        {
+            // Maximises the form whilst retaining the menu bar;
+            this.WindowState = FormWindowState.Maximized;
+
+            InitializeComponent();
+            this.SetStyle(ControlStyles.ResizeRedraw, true);
+            this.BackColor = Color.White;
+
+            // Create the square and circle
+            aSquare = new Rectangle(50, 50, 100, 100);
+            aCircle = new Rectangle(300, 300, 200, 200);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            // Create a pen for drawing
+            Pen blackPen = new Pen(Color.Black);
+            // Draw the square and the circle
+            g.DrawRectangle(blackPen, aSquare);
+            g.DrawEllipse(blackPen, aCircle);
+        }
+
+        //public static void Main()
+        //{
+            //Application.Run(new SquareAndCircle());
+        //}
+    }
+}
